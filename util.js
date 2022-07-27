5750dbcaada9c23fdc6ac109a930f6ae257ad331
fredyen <fredyen@knowhow.fun>
2022-07-28 01:03:05 +0900

    test

--- util.js
+++ util.js
@@ -1,1 +1,17 @@
-console.log("exec at end");
+window.onload = () => {
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+};
+console.log("exec util.js!!");
